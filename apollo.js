0b673c9a8f0f4bcd2d84d7328aecda5bcbca7bb8
Ken Gullaksen <ken.gullaksen@nav.no>
2021-06-03 14:39:08 +0200

    justere scrollbar og åpen state etter skisser (#674)
    * justere scrollbar og åpen state etter skisser
    
    * litt smartere mock og nodemon for autoreload apollo mock

--- apollo.js
+++ apollo.js
@@ -3,23 +3,30 @@ import require from "./esm-require.js";
 import casual from 'casual';
 const {MockList, ApolloServer, gql} = require("apollo-server");
 
+const roundDate = (millis) => {
+    const date = new Date();
+    return new Date(Math.floor(date.getTime() / millis) * millis)
+}
+
 const startApolloMock = () => {
     const data = fs.readFileSync('./bruker.graphql');
     const typeDefs = gql(data.toString());
+    const Beskjed = () => ({
+        __typename: 'Beskjed',
+        merkelapp: casual.word,
+        text: casual.short_description,
+        lenke: `#${casual.word}`,
+    })
     new ApolloServer({
         typeDefs,
         mocks: {
             Query: () =>({
-                notifikasjoner: () => new MockList(200),
+                notifikasjoner: () => new MockList(200, Beskjed),
             }),
             Int: () => casual.integer(0, 1000),
             String: () => casual.string,
-            ISO8601DateTime: () => new Date().toISOString(),
-            Beskjed: () => ({
-                merkelapp: casual.word,
-                text: casual.short_description,
-                lenke: "#foo"
-            }),
+            ISO8601DateTime: () => roundDate(5000).toISOString(),
+            Beskjed,
             Virksomhet: () => ({
                 navn: casual.catch_phrase,
             }),
