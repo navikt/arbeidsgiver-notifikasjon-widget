a3c87ba09899250e69eb268b1b07bcf23ffb4267
Ken Gullaksen <ken.gullaksen@nav.no>
2021-08-06 11:17:32 +0200

    Refactor notifikasjonspanel og oppdater iht design (#724)
    * removed many divs from notifications
    
    * fjern window size calc funksjoner
    
    * fiks casing i datotekst
    
    * forenkle datohåndtering
    
    * oppdater med nyeste svg
    
    * liten forbedring dato
    
    * fiks css og markup for notifikasjoner liste og element
    
    * Fikset det meste av css nå
    
    * Fikset focus og lenke
    
    * flytt event handlere til a element og oppdater aria
    
    * rydd litt i aria stuff
    
    * endret navigasjon slik at tab går in og ut av liste.
    mangler å få fokus på xknapp
    
    * fiks focus og tab navigering
    
    * fiks semantisk navigering og rydd i state handlere
    Ansvar er nå flyttet til riktig sted og vi tar høyde for en fremtidig tabbable footer
    
    * rename til panel og flytt state handler for lukking av panel til widbget
    
    Co-authored-by: Peter Brottveit Bock <peter.brottveit.bock@nav.no>

--- apollo.js
+++ apollo.js
@@ -1,27 +1,62 @@
 import fs from 'fs'
 import require from "./esm-require.js";
 import casual from 'casual';
-const {MockList, ApolloServer, gql} = require("apollo-server");
+const { ApolloServer, gql} = require("apollo-server");
 
 const roundDate = (millis) => {
     const date = new Date();
     return new Date(Math.floor(date.getTime() / millis) * millis)
 }
 
+const casualDate = () => {
+    const date = new Date();
+
+    if (casual.integer(0, 1)) {
+        date.setHours(date.getHours() - casual.integer(0, 60));
+    }
+
+    if (casual.integer(0, 1)) {
+        date.setMinutes(date.getMinutes() - casual.integer(0, 60));
+    }
+
+    if (casual.integer(0, 5)) {
+        date.setDate(date.getDate() - casual.integer(0, 31));
+    }
+
+    if (casual.integer(0, 10) === 0) {
+        date.setMonth(date.getMonth() - casual.integer(0, 12))
+    }
+
+    if (casual.integer(0, 49) === 0) {
+        date.setFullYear(date.getFullYear() - casual.integer(0, 1));
+    }
+    return date;
+};
+
+
 const startApolloMock = () => {
     const data = fs.readFileSync('./bruker.graphql');
     const typeDefs = gql(data.toString());
-    const Notifikasjon = () => ({
-        __typename: casual.boolean ? 'Beskjed' : 'Oppgave',
-        merkelapp: casual.word,
+    const Notifikasjon = (navn) => ({
+        __typename: navn, //casual.boolean ? 'Beskjed' : 'Oppgave',
+        merkelapp: casual.random_element([
+            'Tilskudd',
+            'Rekrutering',
+            'Refusjon',
+            'Sykemeldt'
+        ]),
         text: casual.short_description,
         lenke: `#${casual.word}`,
+        opprettetTidspunkt: casualDate().toISOString(),
     })
     new ApolloServer({
         typeDefs,
         mocks: {
             Query: () =>({
-                notifikasjoner: () => new MockList(200, Notifikasjon),
+                notifikasjoner: () =>
+                    [... new Array(200)]
+                        .map(_ => Notifikasjon(casual.random_element(["Oppgave", "Beskjed"])))
+                        .sort((a, b) => b.opprettetTidspunkt.localeCompare(a.opprettetTidspunkt))
             }),
             Int: () => casual.integer(0, 1000),
             String: () => casual.string,
@@ -29,7 +64,7 @@ const startApolloMock = () => {
             Virksomhet: () => ({
                 navn: casual.catch_phrase,
             }),
-        },
+        }
     }).listen({
         port: 8081,
         path: '/api/graphql',
