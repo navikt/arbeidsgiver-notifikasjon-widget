bd0e59742b57c8b1f05cabae8cfbbc3cae63708f
Ken Gullaksen <ken.gullaksen@nav.no>
2021-08-12 15:15:06 +0200

    fix import i demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,11 +1,11 @@
 //import React from 'react'
 
 // @ts-ignore
-import { ExampleComponent } from '@navikt/arbeidsgiver-notifikasjon-widget'
+import { NotifikasjonWidget } from '@navikt/arbeidsgiver-notifikasjon-widget'
 //import 'arbeidsgiver-notifikasjon-widget/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text={'yas, mhm'} />
+  return <NotifikasjonWidget text={'yas, mhm'} />
 }
 
 export default App
