b874287874f699a061804cb2ffcc744866702d0e
Ken Gullaksen <ken.gullaksen@nav.no>
2021-08-16 15:36:36 +0200

    tweak css til å fungere som embeddet i bedriftsmeny

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,11 +1,18 @@
 //import React from 'react'
 
+import Bedriftsmeny from '@navikt/bedriftsmeny';
+import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 import { NotifikasjonWidget } from '@navikt/arbeidsgiver-notifikasjon-widget'
 import './App.css'
 
 const App = () => {
   return <div className={"typo-normal bakgrunnsside"}>
-    <NotifikasjonWidget apiUri="http://localhost:8081/api/graphql" />
+    <Bedriftsmeny
+      organisasjoner={[]}
+      onOrganisasjonChange={() => null}
+      history={() => {}}>
+      <NotifikasjonWidget apiUri="http://localhost:8081/api/graphql" />
+    </Bedriftsmeny>
   </div>
 }
 
