3e48fc43bff56c3994763068c3384f799b4d64d5
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-08-17 15:12:22 +0200

    Bump virksomhetsvelger. Legg til organisasjoner i virksomhetsvelger.

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,18 +1,23 @@
-//import React from 'react'
-
 import Bedriftsmeny from '@navikt/bedriftsmeny';
 import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 import { NotifikasjonWidget } from '@navikt/arbeidsgiver-notifikasjon-widget'
+import { Router } from 'react-router-dom';
 import './App.css'
+import { createBrowserHistory, History } from 'history';
+import { MOCK_ORGANISASJONER } from './MockOrganisasjoner';
+
+const history: History = createBrowserHistory();
 
 const App = () => {
   return <div className={"typo-normal bakgrunnsside"}>
-    <Bedriftsmeny
-      organisasjoner={[]}
-      onOrganisasjonChange={() => null}
-      history={() => {}}>
-      <NotifikasjonWidget apiUri="http://localhost:8081/api/graphql" />
-    </Bedriftsmeny>
+    <Router history={history}>
+      <Bedriftsmeny
+        organisasjoner={MOCK_ORGANISASJONER}
+        onOrganisasjonChange={() => null}
+        history={history}>
+        <NotifikasjonWidget apiUri="http://localhost:8081/api/graphql" />
+      </Bedriftsmeny>
+    </Router>
   </div>
 }
 
---
+++ demo/src/MockOrganisasjoner.ts
@@ -0,0 +1,172 @@
+import { Organisasjon } from '@navikt/bedriftsmeny/lib/organisasjon';
+
+export const MOCK_ORGANISASJONER: Organisasjon[] = [
+  {
+    Name: 'BALLSTAD OG EIDSLANDET',
+    Type: 'Business',
+    OrganizationNumber: '811076422',
+    ParentOrganizationNumber: '811076112',
+    OrganizationForm: 'AAFY',
+    Status: 'Active'
+  },
+  {
+    Name: 'BALLSTAD OG HAMARØY',
+    Type: 'Business',
+    OrganizationNumber: '811076732',
+    ParentOrganizationNumber: '811076112',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'BALLSTAD OG HORTEN',
+    Type: 'Enterprise',
+    OrganizationNumber: '811076112',
+    ParentOrganizationNumber: '',
+    OrganizationForm: 'FLI',
+    Status: 'Active'
+  },
+  {
+    Name: 'BALLSTAD OG SÆTERVIK',
+    Type: 'Business',
+    OrganizationNumber: '811076902',
+    ParentOrganizationNumber: '811076112',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'BAREKSTAD OG YTTERVÅG REGNSKAP',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '810514442',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'BIRI OG VANNAREID REVISJON',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910998250',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'EIDSNES OG AUSTRE ÅMØY',
+    Type: 'Business',
+    OrganizationNumber: '910521551',
+    ParentOrganizationNumber: '910998250',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'FRØNNINGEN OG LAUVSTAD REVISJON',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910223208',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'HARSTAD OG TYSSEDAL REVISJON',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '810989572',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'HAVNNES OG ÅGSKARDET',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910646176',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'KJØLLEFJORD OG ØKSFJORD',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910175777',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'KYSTBASEN ÅGOTNES OG ILSENG REGNSKAP',
+    Type: 'Enterprise',
+    OrganizationNumber: '910514318',
+    ParentOrganizationNumber: '910175777',
+    OrganizationForm: 'ASA',
+    Status: 'Active'
+  },
+  {
+    Name: 'SANDVÆR OG HOV',
+    Type: 'Business',
+    OrganizationNumber: '910793829',
+    OrganizationForm: 'BEDR',
+    ParentOrganizationNumber: '910720120',
+    Status: 'Active'
+  },
+  {
+    Name: 'SKOTSELV OG HJELSET',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910720120',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'STOL PÅ TORE',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '810771852',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'SØR-HIDLE OG STRAUMGJERDE OG SØNNER OG DØTRE',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910167200',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'TROMVIK OG SPARBU REVISJON',
+    Type: 'Business',
+    OrganizationNumber: '910989626',
+    ParentOrganizationNumber: '910167200',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'Tore sitt testselskap',
+    Type: 'Enterprise',
+    ParentOrganizationNumber: '',
+    OrganizationNumber: '910820834',
+    OrganizationForm: 'AS',
+    Status: 'Active'
+  },
+  {
+    Name: 'UGGDAL OG STEINSDALEN',
+    Type: 'Business',
+    OrganizationNumber: '910521616',
+    ParentOrganizationNumber: '910820834',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'VALESTRANDSFOSSEN OG SØRLI OG SØNN REVISJON',
+    Type: 'Business',
+    OrganizationNumber: '810989602',
+    ParentOrganizationNumber: '910820834',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  },
+  {
+    Name: 'VESTBY OG LOEN OG ALEKSANDERSEN REVISJON',
+    Type: 'Business',
+    OrganizationNumber: '910989642',
+    ParentOrganizationNumber: '910820834',
+    OrganizationForm: 'BEDR',
+    Status: 'Active'
+  }
+];
