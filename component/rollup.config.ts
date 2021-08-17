7b8008f160cb48e6097391a1783aece5f9d11946
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-08-17 16:05:45 +0200

    scope css for widget

--- component/rollup.config.ts
+++ component/rollup.config.ts
@@ -5,43 +5,52 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs"
 import npmImport from "less-plugin-npm-import";
 import url from "@rollup/plugin-url";
+import postcssPrefixSelector from "postcss-prefix-selector"
 
 const svgr = require('@svgr/rollup').default
 const packageJson = require("./package.json");
 
 export default {
-    input: 'src/index.tsx',
-    output: [
-        {
-            file: packageJson.main,
-            format: "cjs",
-            sourcemap: true
-        },
-        {
-            file: packageJson.module,
-            format: "esm",
-            sourcemap: true
-        }
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
 
-    ],
-    plugins: [
-        typescript({ tsconfig: './tsconfig.json' }),
-        commonjs(),
-        resolve(),
-        postcss({
-            use: {
-                less: {
-                    plugins: [new npmImport({prefix: "~"})],
-                },
-                sass: {},
-                stylus: {}
-            }
-        }),
-        url({
-            include: ["**/*.ttf"],
-            limit: Infinity,
-        }),
-        svgr(),
-        peerDepsExternal()
-    ]
+  ],
+  plugins: [
+    typescript({ tsconfig: './tsconfig.json' }),
+    commonjs(),
+    resolve(),
+    postcss({
+      plugins: [
+        postcssPrefixSelector({
+          prefix: '.notifikasjoner_widget',
+          exclude: ['.notifikasjoner_widget']
+        })
+      ],
+      use: {
+        less: {
+          plugins: [
+            new npmImport({prefix: "~"})
+          ],
+        },
+        sass: {},
+        stylus: {}
+      }
+    }),
+    url({
+      include: ["**/*.ttf"],
+      limit: Infinity,
+    }),
+    svgr(),
+    peerDepsExternal()
+  ]
 };
