c5a56bddf52ef5e025ab59b68c2163417536a7f5
bendiksi <bendik.segrov.ibenholt@nav.no>
2021-09-29 15:56:19 +0200

    reformattering

--- component/src/NotifikasjonWidget/AmplitudeProvider.tsx
+++ component/src/NotifikasjonWidget/AmplitudeProvider.tsx
@@ -1,31 +1,30 @@
-
 import React, {createContext, FunctionComponent} from 'react';
 import amplitude from "../utils/amplitude";
 
 type Context = {
-  loggNotifikasjonKlikk: (klikketPaaTidligere:boolean,index:number) => void;
-  loggÅpning: (lesteNotifikasjoner:number,ulesteNotifikasjoner:number) => void;
+  loggNotifikasjonKlikk: (klikketPaaTidligere: boolean, index: number) => void;
+  loggÅpning: (lesteNotifikasjoner: number, ulesteNotifikasjoner: number) => void;
   loggLukking: () => void;
   loggPilTastNavigasjon: () => void;
 
 }
 
 interface Props {
-    lokal: boolean
+  lokal: boolean
 }
 
 export const AmplitudeLoggerContext = createContext<Context>({} as Context);
 
 export const AmplitudeProvider: FunctionComponent<Props> = (props) => {
-    const amplitudeclient = amplitude(props.lokal)
+  const amplitudeclient = amplitude(props.lokal)
 
-  const loggÅpning = (antallNotifikasjoner:number,ulesteNotifikasjoner:number) => {
+  const loggÅpning = (antallNotifikasjoner: number, ulesteNotifikasjoner: number) => {
     amplitudeclient.logEvent('panel-ekspander', {
       tittel: 'arbeidsgiver notifikasjon panel',
       url: window.location.toString(),
-      'antall-notifikasjoner':antallNotifikasjoner,
-      'antall-ulestenotifikasjoner':ulesteNotifikasjoner,
-      'antall-lestenotifikasjoner':antallNotifikasjoner-ulesteNotifikasjoner,
+      'antall-notifikasjoner': antallNotifikasjoner,
+      'antall-ulestenotifikasjoner': ulesteNotifikasjoner,
+      'antall-lestenotifikasjoner': antallNotifikasjoner - ulesteNotifikasjoner,
     });
   }
 
@@ -36,29 +35,29 @@ export const AmplitudeProvider: FunctionComponent<Props> = (props) => {
     });
   }
 
-const loggPilTastNavigasjon = () => {
-  amplitudeclient.logEvent('piltast-navigasjon', {
-    url: window.location.toString(),
-  });
-}
-  const loggNotifikasjonKlikk = (klikketPaaTidligere:boolean,index:number) => {
+  const loggPilTastNavigasjon = () => {
+    amplitudeclient.logEvent('piltast-navigasjon', {
+      url: window.location.toString(),
+    });
+  }
+  const loggNotifikasjonKlikk = (klikketPaaTidligere: boolean, index: number) => {
     amplitudeclient.logEvent('notifikasjon-klikk', {
       url: window.location.toString(),
-      index:index,
+      index: index,
       'klikket-paa-tidligere': klikketPaaTidligere
     });
   }
 
-    let defaultContext: Context = {
-      loggNotifikasjonKlikk: loggNotifikasjonKlikk,
-      loggLukking:loggLukking,
-      loggÅpning:loggÅpning,
-      loggPilTastNavigasjon:loggPilTastNavigasjon
-    };
+  let defaultContext: Context = {
+    loggNotifikasjonKlikk: loggNotifikasjonKlikk,
+    loggLukking: loggLukking,
+    loggÅpning: loggÅpning,
+    loggPilTastNavigasjon: loggPilTastNavigasjon
+  };
 
-    return (
-        <AmplitudeLoggerContext.Provider value={defaultContext}>
-            {props.children}
-        </AmplitudeLoggerContext.Provider>
-    )
+  return (
+    <AmplitudeLoggerContext.Provider value={defaultContext}>
+      {props.children}
+    </AmplitudeLoggerContext.Provider>
+  )
 };
--- component/src/utils/amplitude.ts
+++ component/src/utils/amplitude.ts
@@ -1,28 +1,29 @@
 import amplitude from 'amplitude-js';
+
 const getApiKey = () => {
-    return window.location.hostname === 'arbeidsgiver.nav.no'
-        ? '3a6fe32c3457e77ce81c356bb14ca886'
-        : '55477baea93c5227d8c0f6b813653615';
+  return window.location.hostname === 'arbeidsgiver.nav.no'
+    ? '3a6fe32c3457e77ce81c356bb14ca886'
+    : '55477baea93c5227d8c0f6b813653615';
 };
 
 const createAmpltiudeInstance = () => {
-    const instance = amplitude.getInstance();
-    instance.init(getApiKey(), '', {
-        apiEndpoint: 'amplitude.nav.no/collect',
-        saveEvents: false,
-        includeUtm: true,
-        batchEvents: false,
-        includeReferrer: true
-    });
-    return instance;
+  const instance = amplitude.getInstance();
+  instance.init(getApiKey(), '', {
+    apiEndpoint: 'amplitude.nav.no/collect',
+    saveEvents: false,
+    includeUtm: true,
+    batchEvents: false,
+    includeReferrer: true
+  });
+  return instance;
 }
 
-export default (lokal:boolean): amplitude.AmplitudeClient => {
+export default (lokal: boolean): amplitude.AmplitudeClient => {
   return !lokal ? createAmpltiudeInstance() : {
     logEvent: (event: string, data?: any) => {
       console.log(`${event}: ${JSON.stringify(data)}`, {event, data})
     },
-    setUserProperties:(userProps:object) => {
+    setUserProperties: (userProps: object) => {
       console.log(`set userprops: ${JSON.stringify(userProps)}`)
     }
   } as amplitude.AmplitudeClient
