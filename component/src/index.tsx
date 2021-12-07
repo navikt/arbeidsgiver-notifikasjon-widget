19462b431b906fcf8154065020d5ac1f7e40f4ea
Ken Gullaksen <ken.gullaksen@nav.no>
2021-12-07 11:32:23 +0100

    legg til labs default (#16)

--- component/src/index.tsx
+++ component/src/index.tsx
@@ -5,11 +5,11 @@ import 'nav-frontend-core/dist/main.css'
 import NotifikasjonWidgetComponent from './NotifikasjonWidget/NotifikasjonWidget'
 import { createClient } from './api/graphql'
 import '@navikt/ds-css'
-import { gittMiljo } from './utils/environment'
+import {gittMiljo, Miljø} from './utils/environment'
 
 export type Props = {
   apiUri?: string,
-  miljo: 'local' | 'dev-gcp' | 'prod-gcp';
+  miljo: Miljø;
 }
 
 export const NotifikasjonWidget = (props: Props) => {
@@ -17,12 +17,14 @@ export const NotifikasjonWidget = (props: Props) => {
   const apiurl = props.apiUri ?? gittMiljo({
     prod: 'https://ag-notifikasjon-proxy.nav.no/api/graphql',
     dev: 'https://ag-notifikasjon-proxy.dev.nav.no/api/graphql',
+    labs: 'https://ag-notifikasjon-proxy.labs.nais.io/api/graphql',
     other: 'http://localhost:8081/api/graphql'
   }, props.miljo)
 
   const credentials = gittMiljo({
     prod: 'include',
     dev: 'include',
+    labs: 'include',
     other: undefined
   }, props.miljo)
   return (
