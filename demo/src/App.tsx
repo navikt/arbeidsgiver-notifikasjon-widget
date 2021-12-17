60fbf3bad2a70d9e8971102a987dd2c1189ee7fc
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2021-12-17 16:19:44 +0100

    Eksponer NotifikasjonWidgetProvider for brukere

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,6 @@
 import Bedriftsmeny from '@navikt/bedriftsmeny'
 import '@navikt/bedriftsmeny/lib/bedriftsmeny.css'
-import { NotifikasjonWidget } from '@navikt/arbeidsgiver-notifikasjon-widget'
+import { NotifikasjonWidgetProvider, NotifikasjonWidget } from '@navikt/arbeidsgiver-notifikasjon-widget'
 import { Router } from 'react-router-dom'
 import './App.css'
 import { createBrowserHistory, History } from 'history'
@@ -12,21 +12,23 @@ const history: History = createBrowserHistory()
 const App = () => {
   const [orgname, setOrgname] = useState("")
   return <div className={'typo-normal bakgrunnsside'}>
-    <Router history={history}>
-      <div style={{height: "20em"}}>
-        dekorator
-      </div>
-      <Bedriftsmeny
-        sidetittel={orgname}
-        organisasjoner={MOCK_ORGANISASJONER}
-        onOrganisasjonChange={(org) => setOrgname(org.Name)}
-        history={history}>
-        <NotifikasjonWidget miljo={'local'} />
-      </Bedriftsmeny>
-      <div style={{height: "30em"}}>
-        footer
-      </div>
-    </Router>
+    <NotifikasjonWidgetProvider miljo={'local'}>
+      <Router history={history}>
+        <div style={{height: "20em"}}>
+          dekorator
+        </div>
+        <Bedriftsmeny
+          sidetittel={orgname}
+          organisasjoner={MOCK_ORGANISASJONER}
+          onOrganisasjonChange={(org) => setOrgname(org.Name)}
+          history={history}>
+          <NotifikasjonWidget miljo={"local"}/>
+        </Bedriftsmeny>
+        <div style={{height: "30em"}}>
+          footer
+        </div>
+      </Router>
+    </NotifikasjonWidgetProvider>
   </div>
 }
 
