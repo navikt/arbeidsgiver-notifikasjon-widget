3282f44649de0aea58388da1d1ea84d9d3ac9fed
Ken Gullaksen <ken.gullaksen@nav.no>
2021-12-29 13:46:04 +0100

    fiks demo med cra5

--- demo/craco.config.js
+++ demo/craco.config.js
@@ -1,5 +1,27 @@
 const CracoLessPlugin = require("craco-less");
+const {ProvidePlugin} = require("webpack");
 
 module.exports = {
-    plugins: [{ plugin: CracoLessPlugin }]
+  webpack: {
+    configure: (webpackConfig, {env, paths}) => ({
+      ...webpackConfig,
+      ...{
+        plugins: [
+          ...webpackConfig.plugins,
+          new ProvidePlugin({
+            Buffer: [require.resolve("buffer/"), "Buffer"],
+          })
+        ],
+        resolve: {
+          ...webpackConfig.resolve,
+          fallback: {
+            "buffer": require.resolve("buffer/"),
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+          }
+        }
+      }
+    }),
+  },
+  plugins: [{plugin: CracoLessPlugin}]
 };
